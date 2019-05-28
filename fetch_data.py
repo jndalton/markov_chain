06d2c20d81feb07231cb974747044c5fea528b84
Jack Dalton <jackndalton@icloud.com>
2019-05-28 16:11:51 -0400

    commit all project files, debug majority of program

--- fetch_data.py
+++ fetch_data.py
@@ -1,9 +1,10 @@
+""" This method should draw data from the web and return the data as readable text"""
 
 def fetch_web_data(source):
 	from bs4 import BeautifulSoup
 	import urllib2
 
-	request = urllib2.request(source)
+	request = urllib2.Request(source)
 	response = urllib2.urlopen(request)
 
 	text = response.read()
@@ -12,5 +13,3 @@ def fetch_web_data(source):
 
 	text = BeautifulSoup(text, "html.parser").get_text(strip = True)
 	return text
-
-
--- run.py
+++ run.py
@@ -6,18 +6,17 @@ May 19, 2019.
 """
 
 #Import methods from fetch_data.py and cc_markov.py
-from fetch_data.py import fetch_web_data
-from cc_markov.py import MarkovChain
+import fetch_data
+from markov_python.cc_markov import MarkovChain
 
 mc = MarkovChain()
 
 # links to necessary works
 
-catalog = {
-"Romeo and Juliet": "http://shakespeare.mit.edu/romeo_juliet/full.html",
+catalog = {"Romeo and Juliet": "http://shakespeare.mit.edu/romeo_juliet/full.html",
 "Macbeth": "http://shakespeare.mit.edu/macbeth/full.html",
-"The Bell Jar": "http://gutenberg.ca/ebooks/plaths-belljar/plaths-belljar-00-e.html"
-}
+"The Bell Jar": "http://gutenberg.ca/ebooks/plaths-belljar/plaths-belljar-00-e.html"}
+
 link = ""
 
 """
@@ -28,27 +27,27 @@ and the number of words per passage that the generated strict will have.
 def choose_book():
     print "Which book would you like to see mimicked?"
     print "Enter 1 for Romeo and Juliet, 2 for Macbeth, and 3 for The Bell Jar."
-    choice = ""
-    link = ""
-
-    while choice = "":
-        choice = raw_input("Enter a number:")
-        if choice == 1:
-            print "Romeo and Juliet passage generator initializing..."
-            link = catalog["Romeo and Juliet"]
-        elif choice == 2:
-            print "Macbeth passage generator initializing..."
-            link = catalog["Macbeth"]
-        elif choice == 3:
-            print "The Bell Jar passage generator initializing..."
-            link = catalog["The Bell Jar"]
-        else:
-            choice = ""
+    choice = int(raw_input("Enter a number: "))
+    if choice == 1:
+        print "Romeo and Juliet passage generator initializing..."
+        choice = "Romeo and Juliet"
+        link = catalog["Romeo and Juliet"]
+    elif choice == 2:
+        print "Macbeth passage generator initializing..."
+        choice = "Macbeth"
+        link = catalog["Macbeth"]
+    elif choice == 3:
+        print "The Bell Jar passage generator initializing..."
+        choice = "The Bell Jar"
+        link = catalog["The Bell Jar"]
+    else:
+        print "Not a valid number.  Try again"
+        choose_book()
     return link
 
 def choose_number_passages():
     print "How many passages would you like to have generated?"
-    number_passages = raw_input("Enter a number 1-5: ")
+    number_passages = int(raw_input("Enter a number 1-5: "))
     if number_passages > 0 and number_passages < 6:
         return number_passages
     else:
@@ -57,8 +56,8 @@ def choose_number_passages():
 
 def choose_number_words():
     print "How many words would you like per passage?"
-    number_words = raw_input("Enter a number 4-100")
-    if number_words >= 4 and number words <= 100:
+    number_words = int(raw_input("Enter a number 4-100: "))
+    if number_words >= 4 and number_words <= 100:
         return number_words
     else:
         print "Invalid number of words, try again."
@@ -76,7 +75,7 @@ def generate_passages():
             formatted = " ".join(temp)
             print "Passage #" + str(current+1) + ": " + formatted
             break
-        except UnicodeEncodeError:
+        except UnicodeEncodeError or ValueError:
             pass
 
 generate_passages()
